58a8cc238bd359931908040ac7e4b11bc227394e
leadingtw273 <leadingtw@gmail.com>
2018-08-14 19:41:03 +0800

    分出區網dist

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const socketRouter = require('./routes/socketApi');
 
 const app = express();
 
+let distFile = 'dist';
+// distFile = 'distLocal';
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, distFile)));
 
 app.use('/', indexRouter);
 
