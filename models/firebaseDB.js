450a9858677ed2710a45e9281d96b8bcb5ad731c
leadingtw <leadingtw@gmail.com>
2019-02-19 22:11:27 +0800

    [update] change data fomate

--- models/firebaseDB.js
+++ models/firebaseDB.js
@@ -1,54 +1,65 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://chatcrypt-c2016.firebaseio.com'
+  databaseURL: "https://chatcrypt-c2016.firebaseio.com"
 });
 
-let db = admin.firestore().collection('chatroom');
+let db = admin.firestore().collection("chatroom");
 
 const firebaseDb = {
   readRooms() {
     let read = [];
-    return db.get()
+    return db
+      .get()
       .then(snapshot => {
-        snapshot.forEach((doc) => {
-          read.push(doc.data());
+        snapshot.forEach(doc => {
+          read.push({
+            id: doc.id,
+            ...doc.data()
+          });
         });
         return read;
       })
       .catch(err => err);
   },
   addRoom(data) {
-    return db.add(data)
+    return db
+      .add(data)
+      .then(docRef => docRef.get())
+      .then(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
       .catch(err => err);
   },
   readMsg(data) {
     let read = [];
-    return db.where('id', '==', data.id).get()
+    return db
+      .doc(data.id)
+      .collection("messages")
+      .get()
       .then(snapshot => {
-        let id = 0;
-        snapshot.forEach(doc => id = doc.id);
-        return id;
-      })
-      .then(id => db.doc(id).collection('messages').get())
-      .then(snapshot => {
-        snapshot.forEach(doc => read.push(doc.data()));
+        snapshot.forEach(doc => read.push({ id: doc.id, ...doc.data() }));
         return read;
       })
       .catch(err => err);
   },
   addMsg(data) {
-    return db.where('id', '==', data.roomId).get()
-      .then(snapshot => {
-        let id = 0;
-        snapshot.forEach(doc => id = doc.id);
-        return id;
+    return db
+      .doc(data.roomId)
+      .collection("messages")
+      .add(data.message)
+      .then(docRef => docRef.get())
+      .then(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        };
       })
-      .then(id => db.doc(id).collection('messages').add(data.message))
       .catch(err => err);
-  },
+  }
 };
 
 module.exports = firebaseDb;
--- routes/socketApi.js
+++ routes/socketApi.js
@@ -1,47 +1,49 @@
 const socketApi = io => {
-
-  const express = require('express');
+  const express = require("express");
   const router = express.Router();
+  const fireDB = require("../models/firebaseDB");
 
-  const fireDB = require('../models/firebaseDB');
-
-  io.on('connection', socket => {
-
+  io.on("connection", socket => {
     // 房間獲取
-    socket.on('getRooms', () => {
-      console.log('getRooms');
-      fireDB.readRooms()
-        .then(data => socket.emit('rooms', data))
+    socket.on("getRooms", () => {
+      console.log("getRooms");
+      fireDB
+        .readRooms()
+        .then(data => socket.emit("rooms", data))
         .catch(err => console.log(err));
     });
-    socket.on('setRoom', data => {
-      console.log('setRoom');
-      fireDB.addRoom(data)
-        .then(() => socket.broadcast.emit('pushRoom', data))
+    socket.on("setRoom", data => {
+      console.log("setRoom");
+      fireDB
+        .addRoom(data)
+        .then(room => socket.emit("pushRoom", room))
         .catch(err => console.log(err));
     });
 
     // 訊息獲取
-    socket.on('getMessage', data => {
-      console.log('getMessage');
-      fireDB.readMsg(data)
-        .then(data => socket.emit('messages', data))
+    socket.on("getMessage", data => {
+      console.log("getMessage");
+      fireDB
+        .readMsg(data)
+        .then(msg => {
+          socket.emit("messages", msg);
+        })
         .catch(err => console.log(err));
     });
-    socket.on('setMessage', data => {
-      console.log('setMessage');
-      fireDB.addMsg(data)
-        .then(() => socket.broadcast.emit('pushMessage', data.message))
+    socket.on("setMessage", data => {
+      console.log("setMessage");
+      fireDB
+        .addMsg(data)
+        .then(message => {
+          return socket.emit("pushMessage", message);
+        })
         .catch(err => console.log(err));
     });
 
-    socket.on('disconnect', () => {
-
-    });
+    socket.on("disconnect", () => {});
   });
 
   return router;
-
 };
 
-module.exports = socketApi; 
+module.exports = socketApi;
